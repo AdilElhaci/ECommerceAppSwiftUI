12e4391480e1e9eee59fd2203e9606a814dc9451
adilelhaci <adelalhajji97@gmail.com>
2023-01-03 01:07:01 +0300

    add app icon - change app name

--- EcommerceSwiftUI/ContentView.swift
+++ EcommerceSwiftUI/ContentView.swift
@@ -24,5 +24,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
