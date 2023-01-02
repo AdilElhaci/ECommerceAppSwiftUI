a16f8859321719c1d717d825e4628d4cad39df65
adilelhaci <adelalhajji97@gmail.com>
2023-01-03 00:56:58 +0300

    basic swift ui files

---
+++ EcommerceSwiftUI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  EcommerceSwiftUI
+//
+//  Created by Adil Elhaci on 3.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcommerceSwiftUI/EcommerceSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  EcommerceSwiftUIApp.swift
+//  EcommerceSwiftUI
+//
+//  Created by Adil Elhaci on 3.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
