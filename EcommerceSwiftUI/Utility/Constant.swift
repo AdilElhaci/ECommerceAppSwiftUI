c9760a1b9dc22fb1687f2e1344341e2e0f28d83b
adilelhaci <adelalhajji97@gmail.com>
2023-01-04 00:45:07 +0300

    Constant class added

---
+++ EcommerceSwiftUI/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  EcommerceSwiftUI
+//
+//  Created by Adil Elhaci on 4.01.2023.
+//
+
+import SwiftUI
+
+
+
+//DATA
+
+//COLOR
+let ColorBackground:  Color = Color("ColorBackgournd")
+let ColorGray:  Color = Color(UIColor.systemGray4)
+
+
+
+
+//LAYOUT
+
+//UX
+//API
+//STRING
+//Font
+
+//Image
+
