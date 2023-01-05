9732540b80bcb36630e8d7447396ec8a34848327
adilelhaci <adelalhajji97@gmail.com>
2023-01-06 00:30:31 +0300

    Home page header

--- EcommerceSwiftUI/View/Home/HomePage.swift
+++ EcommerceSwiftUI/View/Home/HomePage.swift
@@ -21,6 +21,7 @@ struct HomePage: View {
                     .padding(.top , UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(Color.white).shadow(color: Color.black.opacity(0.05), radius: 5,x: 0 , y : 5)
                 
+                
                 Spacer()
                 
                 
