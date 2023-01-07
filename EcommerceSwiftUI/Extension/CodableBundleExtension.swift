7565b50d4305db6f3e65bbf9bf7a84a6cf00bb7f
adilelhaci <adelalhajji97@gmail.com>
2023-01-07 22:44:31 +0300

    Add a player model class

--- EcommerceSwiftUI/Extension/CodableBundleExtension.swift
+++ EcommerceSwiftUI/Extension/CodableBundleExtension.swift
@@ -19,19 +19,15 @@ extension Bundle{
         // 2. Create a property for the data
         guard let data = try? Data(contentsOf: url) else{
             fatalError("Failed to load \(file) from bundle.")
-
         }
         
         //3. Create a decoder
         let decoder = JSONDecoder()
         
-        
         //4. Create a property for the decoded data
-        
         guard let decodedData = try? decoder.decode(T.self, from: data)
         else{
             fatalError("Failed to decode \(file) from bundle.")
-
         }
         
         return decodedData
---
+++ EcommerceSwiftUI/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  EcommerceSwiftUI
+//
+//  Created by Adil Elhaci on 7.01.2023.
+//
+
+import Foundation
+
+struct Player : Codable, Identifiable{
+    let id : Int
+    let image : String 
+}
